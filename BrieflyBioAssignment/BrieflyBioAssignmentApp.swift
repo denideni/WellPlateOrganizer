1bac32c085a7295229703274a8ec53b127f7ad9a
Deniz <kecik.deniz@gmail.com>
2023-05-07 13:52:13 -0400

    Initial Commit

---
+++ BrieflyBioAssignment/BrieflyBioAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrieflyBioAssignmentApp.swift
+//  BrieflyBioAssignment
+//
+//  Created by Deniz Kecik on 5/7/23.
+//
+
+import SwiftUI
+
+@main
+struct BrieflyBioAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
